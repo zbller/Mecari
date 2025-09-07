8de295c9033bf792472eda38ae977d853d5e5c0e
zbller <weda@g.ecc.u-tokyo.ac.jp>
2025-09-07 06:56:58 +0000

    port修正

--- app.py
+++ app.py
@@ -171,4 +171,4 @@ with gr.Blocks(theme=gr.themes.Soft(), css=FONT_CSS) as demo:
     _warmup()
 
 if __name__ == "__main__":
-    demo.launch(server_name="0.0.0.0", server_port=int(os.getenv("PORT", "7863")))
+    demo.launch(server_name="0.0.0.0", server_port=int(os.getenv("PORT", "7860")))
